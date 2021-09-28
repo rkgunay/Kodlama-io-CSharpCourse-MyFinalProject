39e5b8cb267d600a8442766b137e7c085c13ed4d
rkgunay <ridvankgunay@gmail.com>
2021-09-29 01:14:17 +0300

    Ders 7 kodları eklendi

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _inMemoryProductDal;
+
+        public ProductManager(IProductDal inMemoryProductDal)
+        {
+            _inMemoryProductDal = inMemoryProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _inMemoryProductDal.GetAll();
+        }
+    }
+}
